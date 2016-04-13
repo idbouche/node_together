c3ac6a874ad8b136ddcb2210132a803f30f15a69
idb <idb@MacBook-Air-de-idb.local>
2016-04-13 17:04:38 +0200

    use packege npm and fill static

--- app/index.js
+++ app/index.js
@@ -1,5 +1,23 @@
-function app (req, res) {
+/*function app (req, res) {
     res.send('Hello World!');
 }
 
-module.exports = app
+module.exports = app*/
+
+
+var express = require('express');
+var app = express();
+
+var tab = [
+    "a","z","e","r"
+]
+
+var motivations = require('motivations');
+var pickOne = require('pick-one');
+
+app.get("/mot", function(req, res){
+
+    res.send(pickOne(motivations));
+
+});
+module.exports = app;
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const port = process.env.PORT || '8080';
 const host = process.env.HOST || '0.0.0.0';
+
 const express = require('express');
 const app = express();
 
-app.use(require('./app'));
+
+var rules = require('./app/index.js');
+app.use(rules)
+//app.use(express.static('site'));
+
+//app.use(require('./app'));
 
 app.listen(port, host);
 
-console.log('Server running %s:%d...', host, port);
+console.log('Server running %s:%d...', host, port, 'i m momo');
 
-module.exports = app;
+//module.exports = app;
